a65398cfda6bc0c4a669b026eb6ccda3e6d71f2d
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2021-05-24 17:39:46 -0500

    add styles

--- src/index.js
+++ src/index.js
@@ -3,21 +3,53 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo";
-window.fetch(url)
-.then(respuesta => respuesta.json())
-.then((responseJoson) => {
-    const todosLosItems = [];
-    responseJoson.data.forEach( (item)  => {
-        const imagen = document.createElement('img');
-        const title = document.createElement('h2');
-        
-        const price = document.createElement('div');
-        
-        const container = document.createElement('div');
+const baseUrl = "https://platzi-avo.vercel.app"
+const url = "https://platzi-avo.vercel.app/api/avo"
 
-        container.append(imagen, title, price);
-        todosLosItems.push(container);
-    });
-    document.body.append(...todosLosItems);
-});
+let app = document.getElementById('app')
+app.className = "foo" //I used grid to display it prettier
+
+const formatPrice = price => {
+
+    const newPrice = new window.Intl.NumberFormat("es-EN", {
+        style: "currency",
+        currency: "USD"
+    }).format(price)
+
+    return newPrice;
+ }
+
+window
+.fetch(url)
+.then(response => response.json())
+.then(responseJson => {
+    const items = []
+    responseJson.data.forEach((item) => {
+        const img = document.createElement('img')
+        img.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+        img.src = `${baseUrl}${item.image}`;
+
+        const title = document.createElement('h2')
+        title.className = "text-lg"
+        title.textContent = item.name
+
+        const price = document.createElement('div')
+        price.className = "text-gray-600"
+        price.textContent = formatPrice(item.price);
+        // price.textContent = item.price
+
+        const priceTitle = document.createElement('div')
+        priceTitle.className = "text-center md:text-left"
+        priceTitle.append(title, price)
+
+        const card = document.createElement("div");
+        card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+        card.append(img, priceTitle)
+
+        const container = document.createElement('div');
+        container.appendChild(card)
+
+        items.push(container)
+    })
+    app.append(...items)
+})
