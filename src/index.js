6e834981d4323a46c9c5c8cc9ac90a1180402b87
Magic-Kowel <58827128+Magic-Kowel@users.noreply.github.com>
2021-05-24 15:00:08 -0500

    make news nodos

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+window.fetch(url)
+.then(respuesta => respuesta.json())
+.then((responseJoson) => {
+    const todosLosItems = [];
+    responseJoson.data.forEach( (item)  => {
+        const imagen = document.createElement('img');
+        const title = document.createElement('h2');
+        
+        const price = document.createElement('div');
+        
+        const container = document.createElement('div');
+
+        container.append(imagen, title, price);
+        todosLosItems.push(container);
+    });
+    document.body.append(...todosLosItems);
+});
